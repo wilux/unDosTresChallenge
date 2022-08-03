b24930a15d6f30f0c817f7c9ca1ae7bf17533d13
Nestor Flores <nestorchoele@gmail.com>
2022-08-03 11:03:38 -0300

    add close driver after test

--- src/test/java/config/BaseTest.java
+++ src/test/java/config/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
@@ -12,38 +13,48 @@ import java.time.Duration;
 public class BaseTest {
     public WebDriver driver;
 
+
     @BeforeTest
     @Parameters("browser")
-    public void setup(String browser) throws Exception{
+    public void setup(String browser) throws Exception {
         //Check if parameter passed from TestNG is 'firefox'
-        if(browser.equalsIgnoreCase("firefox")){
+        if ( browser.equalsIgnoreCase ( "firefox" ) ) {
             //create firefox instance
-            System.setProperty("webdriver.gecko.driver", "webdriver/geckodriver.exe");
-            driver = new FirefoxDriver();
-            driver.manage().timeouts().implicitlyWait( Duration.ofSeconds( 10));
+            System.setProperty ( "webdriver.gecko.driver", "webdriver/geckodriver.exe" );
+            driver = new FirefoxDriver ();
+            driver.manage ().timeouts ().implicitlyWait ( Duration.ofSeconds ( 10 ) );
         }
         //Check if parameter passed as 'chrome'
-        else if(browser.equalsIgnoreCase("chrome")){
+        else if ( browser.equalsIgnoreCase ( "chrome" ) ) {
             //set path to chromedriver.exe
-            System.setProperty("webdriver.chrome.driver","webdriver/chromedriver.exe");
+            System.setProperty ( "webdriver.chrome.driver", "webdriver/chromedriver.exe" );
             //create chrome instance
-            driver = new ChromeDriver();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver = new ChromeDriver ();
+            driver.manage ().timeouts ().implicitlyWait ( Duration.ofSeconds ( 10 ) );
         }
         //Check if parameter passed as 'Edge'
-        else if(browser.equalsIgnoreCase("Edge")){
+        else if ( browser.equalsIgnoreCase ( "Edge" ) ) {
             //set path to Edge driver.exe
-            System.setProperty("webdriver.edge.driver","webdriver/msedgedriver.exe");
+            System.setProperty ( "webdriver.edge.driver", "webdriver/msedgedriver.exe" );
             //create Edge instance
-            driver = new EdgeDriver();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver = new EdgeDriver ();
+            driver.manage ().timeouts ().implicitlyWait ( Duration.ofSeconds ( 10 ) );
         }
-        else{
+        else {
             //If no browser passed throw exception
-            throw new Exception("Browser is not correct");
+            throw new Exception ( "Browser is not correct" );
         }
 
 
     }
+
+
+    @AfterTest
+    public void closeAll() {
+        if ( driver != null ) {
+            driver.quit ();
+        }
+    }
+
 
 }
